058be18b5b823e56d1f2bf0b6317e2b5da0054c9
vrhvid <108895629+vrhvid@users.noreply.github.com>
2023-04-24 15:03:01 +0200

    Spremenjena koda tako, da pošilja številko.

--- bluetooth.ino
+++ bluetooth.ino
@@ -1,12 +1,8 @@
 
 #include <ArduinoBLE.h>
-#include <Arduino_APDS9960.h>
 
 const int UPDATE_FREQUENCY = 2000;     // Update frequency in ms
 
-
-String previousColor = "";
-int r, g, b, a;
 long previousMillis = 0; // last time readings were checked, in ms
 
 BLEService environmentService("181A"); // Standard Environmental Sensing service
@@ -20,11 +16,6 @@ BLEDescriptor colorLabelDescriptor("2901", "16-bit ints: r, g, b, a");
 void setup() {
     Serial.begin(9600); // Initialize serial communication
     // while (!Serial); // only when connected to laptop
-
-    if (!APDS.begin()) { // Initialize APDS9960 sensor
-        Serial.println("Failed to initialize color sensor!");
-        while (1);
-    }
     
     pinMode(LED_BUILTIN, OUTPUT); // Initialize the built-in LED pin
 
@@ -51,6 +42,7 @@ void setup() {
 }
 
 void loop() {
+    int num;
     BLEDevice central = BLE.central(); // Wait for a BLE central to connect
 
     // If central is connected to peripheral
@@ -65,54 +57,13 @@ void loop() {
             // After UPDATE_FREQUENCY ms have passed, check temperature & humidity
             if (currentMillis - previousMillis >= UPDATE_FREQUENCY) {
                 previousMillis = currentMillis;
-                updateReadings();
+                num = 5;
+                colorCharacteristic.writeValue(byte(num), sizeof(num));
             }
         }
 
         digitalWrite(LED_BUILTIN, LOW); // When the central disconnects, turn off the LED
         Serial.print("Disconnected from central MAC: ");
         Serial.println(central.address());
-    }
-}
-
-void getColor() {
-    // check if a color reading is available
-    while (!APDS.colorAvailable()) {
-        delay(5);
-    }
-
-    // Get color as (4) unsigned 16-bit ints
-    int tmp_r, tmp_g, tmp_b, tmp_a;
-    APDS.readColor(tmp_r, tmp_g, tmp_b, tmp_a);
-    r = tmp_r;
-    g = tmp_g;
-    b = tmp_b;
-    a = tmp_a;
-}
-
-void updateReadings() {
-    getColor();
-
-    // Get color reading everytime
-    // e.g. "12345,45678,89012,23456"
-    String stringColor = "";
-    stringColor += r;
-    stringColor += ",";
-    stringColor += g;
-    stringColor += ",";
-    stringColor += b;
-    stringColor += ",";
-    stringColor += a;
-  
-    if (stringColor != previousColor) { // If reading has changed
-
-        byte bytes[stringColor.length() + 1];
-        stringColor.getBytes(bytes, stringColor.length() + 1);
-
-        Serial.print("r, g, b, a: ");
-        Serial.println(stringColor);
-
-        colorCharacteristic.writeValue(bytes, sizeof(bytes));
-        previousColor = stringColor;
     }
 }
